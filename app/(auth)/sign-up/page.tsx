09f99f6d05f271876480a346ce511b24599510d9
joey <joeytitanium@gmail.com>
2025-02-07 13:29:26 +0900

    split sign-up page into client/server

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,16 +1,8 @@
 'use client';
 
-import { AuthContainer } from '@/app/(auth)/_components/auth-container';
-import { SignupForm } from '@/app/(auth)/_components/sign-up-form';
-import { SocialLogin } from '@/app/(auth)/_components/social-login';
-import { usePostSignInUp } from '@/app/(auth)/use-post-sign-in-up';
-import { getRouteUrl } from '@/routing/get-route-url';
-import { Anchor, Divider, Text, Title } from '@mantine/core';
-import { showNotification } from '@mantine/notifications';
-import NextLink from 'next/link';
-import { useSearchParams } from 'next/navigation';
 import { Suspense } from 'react';
 import { z } from 'zod';
+import { SignUpPage } from './client';
 
 const SCHEMA = z
   .object({
@@ -20,56 +12,14 @@ const SCHEMA = z
     variantId: Number(data.variantId),
   }));
 
-const SignUpPage = () => {
-  const searchParams = useSearchParams();
+const Page = ({ searchParams }: { searchParams: { variantId: string } }) => {
   const query = SCHEMA.safeParse(searchParams);
-  const { handleSignInUp } = usePostSignInUp({
-    variantId: query.data?.variantId,
-    routeTo: getRouteUrl({ to: '/pricing' }),
-  });
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <AuthContainer
-        bottomSection={
-          <Text ta="center" mt="xl">
-            Already have an account?{' '}
-            <Anchor
-              component={NextLink}
-              href={getRouteUrl({
-                to: '/sign-in',
-                params: { variantId: query.data?.variantId.toString() },
-              })}
-            >
-              Sign-in
-            </Anchor>
-          </Text>
-        }
-      >
-        <Title mb="lg">Sign-up</Title>
-        <SocialLogin
-          type="signup"
-          onSuccess={async ({ user }) => {
-            await handleSignInUp({ user });
-          }}
-        />
-        <Divider mt="xl" mb="lg" label="Or" />
-        <SignupForm
-          initialEmailValue=""
-          emailRedirectTo={getRouteUrl({ to: '/pricing' }, { absoluteUrl: true })}
-          onSuccess={async ({ user }) => {
-            await handleSignInUp({ user });
-          }}
-          onError={(error) => {
-            showNotification({
-              message: error.message,
-              color: 'red',
-            });
-          }}
-        />
-      </AuthContainer>
+      <SignUpPage variantId={query.data?.variantId} />
     </Suspense>
   );
 };
 
-export default SignUpPage;
+export default Page;

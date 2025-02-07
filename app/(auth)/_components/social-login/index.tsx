5fd4af94dc4d697fd23972137b32baaa05552dfe
joey <joeytitanium@gmail.com>
2025-02-07 13:52:53 +0900

    fix social login button width

--- app/(auth)/_components/social-login/index.tsx
+++ app/(auth)/_components/social-login/index.tsx
@@ -2,6 +2,8 @@ import { handleSignUp } from '@/app/(auth)/_utils/handle-sign-up';
 import { CONFIG } from '@/config';
 import { supabaseClient } from '@/lib/supabase/client';
 import { logError } from '@/utils/logger';
+import { Box } from '@mantine/core';
+import { useElementSize } from '@mantine/hooks';
 import { notifications } from '@mantine/notifications';
 import { GoogleLogin } from '@react-oauth/google';
 import { Session, User } from '@supabase/supabase-js';
@@ -13,57 +15,63 @@ type Props = {
 
 export const SocialLogin = ({ type, onSuccess }: Props) => {
   const supabase = supabaseClient();
+  const { ref, width } = useElementSize();
 
   return (
-    <GoogleLogin
-      theme="filled_black"
-      use_fedcm_for_prompt
-      text={type === 'signin' ? 'signin_with' : 'signup_with'}
-      login_uri={CONFIG.auth.google.callbackUrl}
-      onSuccess={async (response) => {
-        if (!response.credential) {
-          console.error(`Missing credential: ${JSON.stringify(response, null, '\t')}`);
+    <Box w="100%" ref={ref}>
+      <GoogleLogin
+        width={width}
+        use_fedcm_for_prompt
+        text={type === 'signin' ? 'signin_with' : 'signup_with'}
+        login_uri={CONFIG.auth.google.callbackUrl}
+        onSuccess={async (response) => {
+          if (!response.credential) {
+            logError({
+              message: 'Missing Google credential',
+              context: { response },
+            });
+            notifications.show({
+              message: 'Error signing in with Google',
+              color: 'red',
+            });
+            return;
+          }
+
+          const { data: signInData, error: signInError } = await supabase.auth.signInWithIdToken({
+            provider: 'google',
+            token: response.credential,
+          });
+
+          if (signInError) {
+            logError({ message: 'Error signing in with Google', error: signInError });
+            notifications.show({
+              message: 'Error signing in with Google',
+              color: 'red',
+            });
+            return;
+          }
+          const { user } = signInData;
+
+          await handleSignUp({
+            email: user.email ?? 'missing email',
+            name: user.user_metadata.name,
+            source: 'Google',
+            userId: user.id,
+            avatarUrl: user.user_metadata.avatar_url,
+          });
+
+          onSuccess(signInData);
+        }}
+        onError={() => {
+          logError({
+            message: 'Error signing in with Google',
+          });
           notifications.show({
             message: 'Error signing in with Google',
             color: 'red',
           });
-          return;
-        }
-
-        const { data: signInData, error: signInError } = await supabase.auth.signInWithIdToken({
-          provider: 'google',
-          token: response.credential,
-        });
-
-        if (signInError) {
-          console.error('Error signing in with Google', signInError.message);
-          notifications.show({
-            message: 'Error signing in with Google',
-            color: 'red',
-          });
-          return;
-        }
-        const { user } = signInData;
-
-        await handleSignUp({
-          email: user.email ?? 'missing email',
-          name: user.user_metadata.name,
-          source: 'Google',
-          userId: user.id,
-          avatarUrl: user.user_metadata.avatar_url,
-        });
-
-        onSuccess(signInData);
-      }}
-      onError={() => {
-        logError({
-          message: 'Error signing in with Google',
-        });
-        notifications.show({
-          message: 'Error signing in with Google',
-          color: 'red',
-        });
-      }}
-    />
+        }}
+      />
+    </Box>
   );
 };
